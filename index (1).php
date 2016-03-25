3a8273c7085b75bd8be1673130404eafc3f59e71
Martin Bell <martin@backhandmedia.com>
2016-03-25 11:11:53 -0700

    First commit

---
+++ index (1).php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head profile='http://www.w3.org/2006/03/hcard'>
+  <title>Martin Bell: Online Marketing, Web Analytics, SEO, Web Design and Development, Production &amp; Project Management - Napa CA</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+  <meta name="keywords" content="web design, analytics SEO, search engine optimization, online marketing, napa, napa ca," />
+  <meta name="description" content="The online home of Martin Bell." />
+<meta name="verify-v1" content="n56saWRfTDNKvRv8/GTedJ7yQd0avjvwgajU3FEvah0=" />
+  <meta name="y_key" content="673a65ed10a0a06b" />
+  <link rel="stylesheet" href="style.css" type="text/css" />
+  <link rel="shortcut icon" type="image/x-icon" href="images/red-attack_16.ico" />
+  <link rel="alternate" type="application/atom+xml" href="http://friendfeed.com/?format=atom&auth=3DPMP0ZMt8RwjpcX" title="Martin's shared articles" />
+<script type="text/javascript" src="scripts/jQuery.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-275020-4']);
+  _gaq.push(['_setDomainName', 'backhandmedia.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+<div id="container" class="vcard">
+	<div id="header">
+	<h1 class="fn"><a href="http://www.backhandmedia.com" class="url" rel="me">Martin Bell</a></h1>
+	<h2 class="title">Online Marketing, Web Analytics, SEO, Web Design and Development, Production &amp; Project Management</h2>
+	<address>Napa CA </address>
+	<a id="feed" href="http://friendfeed.com/?format=atom&auth=3DPMP0ZMt8RwjpcX" title="Read what I'm reading...">Read what I'm reading...</a>
+	</div>
+	<div id="breadcrumbs">
+		<p class="blog">Read my <a href="http://blog.backhandmedia.com/">blog</a></p>
+		<p class="resume">Read my <a href="resume/index.php">resume</a></p>
+		<p class="contactMe"><a href="mailto: martin@backhandmedia.com" title="email me">contact me</a></p>
+	</div>	
+<?php include("includes/portfolio.php");?>
+<div id="footer">
+<h4>a backhand media project</h4>
+<p>Money can't buy you happiness but it can buy you assassins</p>
+
+</div>
+</div>
+
+</body>
+</html>

779d0a54e7321a05f60e99757719bc4f101432be
Martin Bell <martin@backhandmedia.com>
2016-07-07 17:06:14 -0700

    more styles

--- backhand/index.php
+++ backhand/index.php
@@ -1,16 +1,15 @@
 <?php get_header(); ?>
-<div class="container">
+<div class="container" id="home">
 	<div class="jumbotron">
-		<h2 id="about">Backhand Stories is a <strong>creative writing blog</strong> that publishes new <a href="<?php echo get_option('home'); ?>/fiction">short stories</a>, flash fiction, <a href="<?php echo get_option('home'); ?>/non-fiction">non-fiction</a> and <a href="<?php echo get_option('home'); ?>/essays">essays</a> by new and unpublished writers. <a id="submit"href="http://www.backhandstories.com/submission-guidelines/">Submit</a> your own short story!</h2>
+		<h2>Backhand Stories is a <strong>creative writing blog</strong> that publishes new <a href="<?php echo get_option('home'); ?>/fiction">short stories</a>, flash fiction, <a href="<?php echo get_option('home'); ?>/non-fiction">non-fiction</a> and <a href="<?php echo get_option('home'); ?>/essays">essays</a> by new and unpublished writers. <a id="submit"href="http://www.backhandstories.com/submission-guidelines/">Submit</a> your own short story!</h2>
 	</div>
 	<div class="row">
 		<?php if (have_posts()) : ?>
 		<?php while (have_posts()) : the_post(); ?>
-		<article class="post col-md-6" id="post-<?php the_ID(); ?>">
+		<article class="post col-md-4" id="post-<?php the_ID(); ?>">
 			<h2><?php the_category(', ') ?>: <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 			<div class="entry">
 				<?php the_content('Read the rest of this story &raquo;'); ?>
-				<p class="postmetadata"><!-- google_ad_section_start --><?php the_tags('Tags: ', ', ', '<br />'); ?> Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p><!-- google_ad_section_end -->
 			</div>
 		</article>
 		<?php endwhile; ?>

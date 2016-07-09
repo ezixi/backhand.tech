9240b3ba7fc00ef0f45b592c1f3aaa313c531951
Martin Bell <martin@backhandmedia.com>
2016-07-08 17:25:31 -0700

    footer

--- backhand/footer.php
+++ backhand/footer.php
@@ -1,21 +1,22 @@
 <footer>
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
             <div class="col-md-6">
                 <ul>
-                <?php wp_list_pages('title_li=' ); ?>
+                    <?php wp_list_pages('title_li=' ); ?>
                 </ul>
             </div>
-            <div class="col-md-6">
-            <div class="row">
+            <div class="col-md-6" >
+                <a class="social" href="https://www.facebook.com/BackhandStories/" onclick="return !window.open(this.href, 'Facebook', 'width=1000,height=600')" target="_blank"><i class="fa fa-3x fa-facebook"></i></a>
+                <a class="social" href="https://twitter.com/intent/follow?screen_name=@backhandstories" onclick="return !window.open(this.href, 'Twitter', 'width=600,height=500')" target="_blank"><i class="fa fa-3x fa-twitter"></i></a>
+                <p><a href="http://www.backhandmedia.com">A Backhand Media Project</a></p>
             <p>&copy; Backhand Media. All work remains the property of the author unless otherwise stated.</p>
-            <p><a href="http://www.backhandmedia.com">A Backhand Media Project</a></p>
-            <?php wp_footer(); ?>
-        </div>
             </div>
         </div>
 
     </div>
+</div>
 </footer>
 </body>
 </html>
+
--- backhand/header.php
+++ backhand/header.php
@@ -15,6 +15,7 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <script src="https://use.fontawesome.com/73f45f37af.js"></script>
         <title><?php wp_title(); ?> Short stories, flash fiction and creative writing online. </title>
         <meta name="description" content="<?php wp_title(); ?>" />
         <meta name="keywords" content="short story, creative writing, short stories, fiction, short fiction, non-fiction stories, short story blog, creative writing blog, submit a story, new writers" />
@@ -30,7 +31,7 @@
         <meta property="fb:admins" content="618395819"/>
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@backhandstories" />
-        <meta name="twitter:creator" content="@backhandmedia" />
+        <meta name="twitter:creator" content="@backhand" />
         <meta name="twitter:title" content="<?php wp_title(); ?>" />
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
         <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />

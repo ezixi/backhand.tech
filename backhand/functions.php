98eb704a03e70c9de8e397b07cbaf227c8ebd126
Martin Bell <martin@backhandmedia.com>
2016-08-10 16:06:55 -0700

    moves contant ad up to improve viewability

--- backhand/functions.php
+++ backhand/functions.php
@@ -86,7 +86,7 @@ function prefix_insert_post_ads( $content ) {
         </div>';
 
     if ( is_single() && ! is_admin() ) {
-        return prefix_insert_after_paragraph( $ad_code, 5, $content );
+        return prefix_insert_after_paragraph( $ad_code, 1, $content );
     }
     
     return $content;

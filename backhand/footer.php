72a2ce21889a259fbf6117930041da1098eba828
Martin Bell <martin@backhandmedia.com>
2016-10-13 09:42:53 -0700

    adds tumblr link

--- backhand/footer.php
+++ backhand/footer.php
@@ -9,6 +9,7 @@
             <div class="col-md-6" >
                 <a class="social" href="https://www.facebook.com/BackhandStories/" onclick="return !window.open(this.href, 'Facebook', 'width=1000,height=600')" target="_blank"><i class="fa fa-3x fa-facebook"></i></a>
                 <a class="social" href="https://twitter.com/intent/follow?screen_name=@backhandstories" onclick="return !window.open(this.href, 'Twitter', 'width=600,height=500')" target="_blank"><i class="fa fa-3x fa-twitter"></i></a>
+                <a class="social" href="https://www.tumblr.com/follow/backhandstories" onclick="return !window.open(this.href, 'Tumblr', 'width=600,height=500')" target="_blank"><i class="fa fa-3x fa-tumblr"></i></a>
                 <p><a href="http://www.backhandmedia.com">A Backhand Media Project</a></p>
                 <p>&copy; Backhand Media. All work remains the property of the author unless otherwise stated.</p>
             </div>

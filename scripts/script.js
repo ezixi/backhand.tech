cfab0c9f5680e42654c066390f09a03bf1759f97
Martin Bell <martin@backhand.tech>
2020-07-14 16:58:39 +0100

    adds event label

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ $( document ).ready(function() {
     $("a").each(function() {
         var href = $(this).attr('href');
         var target = $(this).attr('target');
-        var text = $(this).attr('title');
+        var text = $(this).text();
         $(this).click(function(event) { // when someone clicks these links
             event.preventDefault(); // don't open the link yet
-           gtag('event', 'select_content', text); // create a custom event
+           gtag('event', 'select_content', { 'event_label' : text }); // create a custom event
             setTimeout(function() { // now wait 300 milliseconds...
                 window.open(href,(!target?"_self":target)); // ...and open the link as usual
             },300);

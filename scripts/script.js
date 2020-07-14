c9b0e71d31be4f100bf2f53253ae20d6abdf17af
Martin Bell <martin@backhand.tech>
2020-07-14 16:22:09 +0100

    removes absolute links for local dev

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
     $( ".blog_content img" ).addClass( "img-fluid rounded mx-auto" );
     $( ".blog_content h1" ).addClass( "display-4" );
+
+        //analytics
+    $("a").each(function() {
+        var href = $(this).attr('href');
+        var target = $(this).attr('target');
+        var text = $(this).attr('title');
+        $(this).click(function(event) { // when someone clicks these links
+            event.preventDefault(); // don't open the link yet
+           gtag('event', 'select_content', text); // create a custom event
+            setTimeout(function() { // now wait 300 milliseconds...
+                window.open(href,(!target?"_self":target)); // ...and open the link as usual
+            },300);
+        });
+    });
 });
